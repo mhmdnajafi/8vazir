2672720dae269d4d55b66397e7b43fec1d7dd69f
mhmdnajafi <muhammadnajafi78@gmail.com>
2023-05-23 00:23:13 +0330

    8_vazir _object

---
+++ project_8vazir.py
@@ -0,0 +1,63 @@
+import numpy as np
+import random
+class Gentic():
+    def __init__(self,parentnumber,crossoverpercent,mutationpercent):
+        self.parentnumber = parentnumber
+        self.crossoverpercent=crossoverpercent
+        self.mutationpercent=mutationpercent
+    
+
+    def generation(self):
+        parentlist= list()
+        for i in range(self.parentnumber):
+           parent = list(range(1,9))
+           np.random.shuffle(parent)
+           parentlist.append(parent)
+        return parentlist
+    
+    def crossover(self,parentlist):
+        listnumber=list(range(0,9))
+        crossover=list()
+        for i in range(int(self.parentnumber*self.crossoverpercent/2)):
+            crossoverlist=random.choices(parentlist, k=2)
+            randomnumber= random.choice(listnumber)
+            child1=crossoverlist[0][:randomnumber] + crossoverlist[1][randomnumber:]
+            child2=crossoverlist[1][:randomnumber] + crossoverlist[0][randomnumber:]
+            crossover.append(child1)
+            crossover.append(child2)
+        return crossover
+    
+    def mutation(self,crossover):
+        listnumber=list(range(0,8))
+        select_for_mutation= random.choices(crossover, k = int(self.crossoverpercent*len(crossover)))
+        mutationlist=list()
+        for i in select_for_mutation:
+            randomnumber_index= random.choice(listnumber)
+            randomnumber_value= random.choice(listnumber)
+            i[randomnumber_index]=randomnumber_value
+            mutationlist.append(i)
+        return mutationlist
+    
+#    def cost(self):
+    
+#    def gentic():
+    
+
+
+a=Gentic(10,0.8,0.3)
+parent=a.generation()
+print(parent)
+print("***************************************")
+print("***************************************")
+print("***************************************")
+crossover=a.crossover(parent)
+print(crossover)
+print("***************************************")
+print("***************************************")
+print("***************************************")
+mutation = a.mutation(crossover)
+print(mutation)
+
+
+
+
