a240ef47b934da95af4d853cf24419302c57c095
mhmdnajafi <muhammadnajafi78@gmail.com>
2023-05-28 00:25:29 +0330

    Output

--- Runner.py
+++ Runner.py
@@ -1,6 +1,6 @@
 from NQueen import Gentic
 queen=Gentic(7,500,0.8,0.2)
-run_l,answer_l,time_l,answer=queen.run(15000)
+run_l,answer_l,time_l,answer=queen.run(30000)
 path_save=queen.folder('7 Queen')
 queen.chart(run_l,answer_l,'Run','True Answer',show=False,path=path_save,save=True)
 queen.chart(time_l[2:],answer_l,'Time','True Answer',show=False,path=path_save,save=True)
